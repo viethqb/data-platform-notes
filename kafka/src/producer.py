00333550019289af41b5bb6fa8ba29d4ee53a835
viethq <hoangquocvietbk2.13@gmail.com>
2024-07-31 10:20:46 +0700

    edit producer.py

--- kafka/src/producer.py
+++ kafka/src/producer.py
@@ -1,21 +1,38 @@
-import logging
-import time
-import pandas as pd
+from time import time
 from kafka import KafkaProducer
+from faker import Faker
+import json, time
+import os
 
-logging.basicConfig(level=logging.INFO)
-
-producer = KafkaProducer(
-    bootstrap_servers="172.25.0.2:32100,172.25.0.3:32100,172.25.0.4:32100"
+BOOTSTRAP_SERVERS = os.getenv(
+    "BOOTSTRAP_SERVERS", "172.25.0.2:32100,172.25.0.3:32100,172.25.0.4:32100"
 )
 
+producer = KafkaProducer(bootstrap_servers=BOOTSTRAP_SERVERS)
 
-total_processed = 0
-i = 1
-df = pd.read_csv("./src/yellow_tripdata.csv")
+faker = Faker()
 
-while True:
-    for index, row in df.sample(n=1000).iterrows():
-        producer.send("my-topic", bytes(row.to_json(), "utf-8"))
-    producer.flush()
+
+def get_register():
+    return {"name": faker.name(), "add": faker.year()}
+
+
+def get_partitioner(key_bytes, all_partitions, available_partitions):
+    return 0
+
+
+def json_serializer(data):
+    return json.dumps(data).encode("utf-8")
+
+
+producer = KafkaProducer(
+    bootstrap_servers=BOOTSTRAP_SERVERS,  # server name
+    value_serializer=json_serializer,  # function callable
+    # partitioner = get_partitioner, # function return 0 >>> only partition_0 can received messages
+)
+
+while 1 == 1:
+    user = get_register()
+    print(user)
+    producer.send("users", user)
     time.sleep(1)
